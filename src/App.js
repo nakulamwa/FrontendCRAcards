d5b62d35fd50c1d55a41d51fb36c5aa5b79ab862
unknown <nakula.codes@gmail.com>
2024-04-11 16:06:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import IDCard from './components/IDCard';
+import Data from "./Data.json";
 
 function App() {
+  console.log(Data.data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1> Welcome to IDCards App! </h1>  
+
+
+          {/* <div className='d-flex'>
+          <IDCard iname="Jane" idesc="Likes coding. likes travelling"/>  
+          <IDCard iname="John" idesc="Likes analyzing. likes swimming"/>  
+          <IDCard iname="Mike" idesc="Likes cooking. likes hiking"/>        
+          </div>
+           */}
+
+
+        <div className='d-flex'>
+          {
+            Data.data.map( (elem)=> {
+               return <IDCard iname= {elem.name} idesc={elem.des} />
+            }
+            )
+          }
+        </div>
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
 
+
 export default App;
+
